f102c96bfe4084b98ba70fc9d767b323bdc22a98
arenas-franklin <franklin.silva8@fatec.sp.gov.br>
2020-03-09 16:02:32 -0300

    implementado banco de dados

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //IMPORTACOES 
 const express = require("express")
+const bodyParser = require("body-parser")
 const routers = require('./routers')
 
 //INSTANCIA DO FRAMEWORK EXPRESS
@@ -12,6 +13,9 @@ app.set("views", __dirname + "/src/views")
 
 //CONFIGURANDO CONTEUDO STATICS
 app.use(express.static(__dirname + "/public"))
+
+//configurar o request body
+app.use(bodyParser.urlencoded({extended:true}))
 
 //ROTAS DA APLICAÇÂO
 app.use(routers)
